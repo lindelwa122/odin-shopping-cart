e196ecb2b9674df8794e9ced20beeded3bbe08e9
Lindelwa <lindelwa.codes@gmail.com>
2024-01-31 09:03:44 +0200

    Format using prettier and use app.jsx instead of router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Router from './router';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>,
-)
+);
