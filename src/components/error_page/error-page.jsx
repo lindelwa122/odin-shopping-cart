32f5843f9492bca62fe3a64cec3f654f3a0a7edc
Lindelwa <lindelwa.codes@gmail.com>
2024-02-04 17:03:53 +0200

    Add error page.

---
+++ src/components/error_page/error-page.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import Navbar from '../header/navbar';
+import styles from './error-page.module.css';
+
+const ErrorPage = ({ error }) => {
+  console.error(error);
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.page}>
+        <h1>{
+          error ?? 'There was an error. Check your internet connection and try going to the home page.'
+        }</h1>
+      </div>
+    </>
+  )
+}
+
+ErrorPage.PropTypes = {
+  error: PropTypes.string,
+};
+
+export default ErrorPage;
